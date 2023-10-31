707d9d830fc7adfe746e38dbb1a2597166e3bf5a
Nolin McFarland <nolin.mcfarland@icloud.com>
2023-10-31 07:45:28 -0400

    Add creating and removing todos

--- index.js
+++ index.js
@@ -12,9 +12,40 @@ function App() {
             title: "todo 3",
             isComplete: false,
         },
-    ])
+    ]);
+    const [value, setValue] = React.useState('');
+    
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (!value) return;
+        const newTodos = [...todos, {title: value, isComplete: false}];
+        setTodos(newTodos);
+        setValue('');
+    };
+    
+    const removeTodo = e => {
+        const index = Number(e.target.id);
+        let newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
     return (<>
-        {todos.map((todo, index) => <div className="todo" key={index}>{todo.title}</div>)}
+        {todos.map((todo, index) => <div
+            className="todo"
+            key={index}
+            id={index}
+            onClick={removeTodo}
+        >{todo.title}</div>)}
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                placeholder="Add todo..."
+                onChange={e => setValue(e.target.value)}
+            />
+        </form>
     </>);
 }
 
